25b3d7b1998fa259857886104302ed21bc92cdb7
Jumpei Ogawa <phanect@users.noreply.github.com>
2023-09-25 20:48:35 +0900

    feat: separate entry points for `<script>` and npm module

--- src/embed.ts
+++ src/embed.ts
@@ -2,90 +2,11 @@
  * @file Entry for embed.js
  */
 
-import * as maplibregl from 'maplibre-gl';
-import GeoloniaMap from './lib/geolonia-map';
-import GeoloniaMarker from './lib/geolonia-marker';
-import { SimpleStyle } from './lib/simplestyle';
-import { VERSION as embedVersion } from './version';
-import { registerPlugin, renderGeoloniaMap } from './lib/render';
-
-export type { GeoloniaMapOptions } from './lib/geolonia-map';
-
-export type Popup = maplibregl.Popup;
-
-export type EmbedAttributes = {
-  lat: string;
-  lng: string;
-  zoom: string;
-  bearing: string;
-  pitch: string;
-  hash: string;
-  marker: string;
-  markerColor: string;
-  openPopup: string;
-  customMarker: string;
-  customMarkerOffset: string;
-  gestureHandling: string;
-  navigationControl: string;
-  geolocateControl: string;
-  fullscreenControl: string;
-  scaleControl: string;
-  geoloniaControl: string;
-  geojson: string;
-  cluster: string;
-  clusterColor: string;
-  style: string;
-  lang: string;
-  plugin: string;
-  key: string;
-  apiUrl: string;
-  loader: string;
-  minZoom: string;
-  maxZoom: string;
-  '3d': string;
-  [otherKey: string]: string;
-};
-
-export type EmbedPlugin<PluginAttributes extends { [otherKey: string]: string } = {}> = (map: GeoloniaMap, target: HTMLElement, atts: EmbedAttributes & PluginAttributes) => void;
-
-// Type for `window.geolonia`
-export type Geolonia = Partial<typeof maplibregl> & {
-  accessToken?: string;
-  embedVersion?: string;
-  Map?: typeof GeoloniaMap;
-  Marker?: typeof GeoloniaMarker;
-  SimpleStyle?: typeof SimpleStyle;
-  simpleStyle?: typeof SimpleStyle; // backward compatibility
-  registerPlugin?: (embedPlugin: EmbedPlugin) => void;
-};
-
-declare global {
-  interface Window {
-    geolonia: Geolonia,
-    maplibregl?: Geolonia,
-    mapboxgl?: Geolonia,
-  }
-}
-
-const geolonia: Geolonia = Object.assign(window.geolonia || {}, maplibregl, {
-  Map: GeoloniaMap,
-  Marker: GeoloniaMarker,
-  SimpleStyle: SimpleStyle,
-  simpleStyle: SimpleStyle,
-  embedVersion,
-  registerPlugin,
-});
+import { geolonia } from './module';
+import { renderGeoloniaMap } from './lib/render';
 
 window.geolonia =
   window.maplibregl =
   window.mapboxgl = geolonia;
 
 renderGeoloniaMap();
-
-export {
-  geolonia,
-  GeoloniaMap as Map,
-  GeoloniaMarker as Marker,
-  SimpleStyle,
-  embedVersion,
-};
